6d6dfbc866d7c185cc2f48996b785a20197c8c04
bright_spark <markpassey@btinternet.com>
2017-04-17 01:12:47 +0100

    Setup basic environment

---
+++ src/main/java/brightspark/structuralrelocation/StructuralRelocation.java
@@ -0,0 +1,57 @@
+package brightspark.structuralrelocation;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class StructuralRelocation
+{
+    public static final String MOD_ID = "structuralrelocation";
+    public static final String MOD_NAME = "Structural Relocation";
+    public static final String VERSION = "1.10.2-0.0.1";
+    public static final String GUI_TEXTURE_DIR = "textures/gui/";
+
+    @Mod.Instance(MOD_ID)
+    public static StructuralRelocation instance;
+
+    public static final CreativeTabs SR_TAB = new CreativeTabs(MOD_ID)
+    {
+        @Override
+        public Item getTabIconItem()
+        {
+            return Items.ENDER_PEARL;
+        }
+
+        @Override
+        public String getTranslatedTabLabel()
+        {
+            return MOD_NAME;
+        }
+    };
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        //Initialize item, blocks, textures/models and configs here
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        //Initialize GUIs, tile entities, recipies, event handlers here
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        //Run stuff after mods have initialized here
+
+    }
+
+}
