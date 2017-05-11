c2f15a1e5f0ec53510fc572093b984aae49112a0
bright_spark <markpassey@btinternet.com>
2017-05-11 13:59:39 +0100

    Set default max area size config to 64

--- src/main/java/brightspark/structuralrelocation/Config.java
+++ src/main/java/brightspark/structuralrelocation/Config.java
@@ -5,5 +5,5 @@ public class Config
     /**
      * The maximum size of a dimension of a selected area
      */
-    public static int maxTeleportAreaSize = 5;
+    public static int maxTeleportAreaSize = 64;
 }
